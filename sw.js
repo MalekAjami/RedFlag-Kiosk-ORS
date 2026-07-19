63d2f2ac2f231f69a1c67142fcce9fd489b52a4b
MalekAjami <49150591+MalekAjami@users.noreply.github.com>
2026-07-19 05:23:08 +0200

    Add files via upload
    Rapport+FicheF2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'redflag-ors-cache-v4';
+const CACHE_NAME = 'redflag-ors-cache-v5';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
